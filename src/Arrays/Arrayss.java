e11fb2f4f4d63f1886e3a426357fad5a2369bc95
Marya <maryaoaidain@gmail.com>
2025-11-30 15:14:46 +0300

    v2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -2,9 +2,10 @@ package Arrays;
 
 import java.util.Scanner;
 
-public class Arrayss {
+public class Arrayss{
 
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
+
     public void Traversal(int[] a) {
 
         System.out.println("the elements are:");
@@ -25,9 +26,49 @@ public class Arrayss {
         for (int i = a.length - 1; i >= 0; i--)
             System.out.println("Index [" + i + "] = " + a[i]);
     }
-    public void input(int[]a){
-        System.out.println("Enter"+a.length+" numbers:");
-        for(int i=0 ; i<a.length;i++)
-            a[i]=scanner.nextInt();
+
+    public void input(int[] a) {
+        System.out.println("Enter" + a.length + " numbers:");
+        for (int i = 0; i < a.length; i++)
+            a[i] = scanner.nextInt();
     }
+
+    public void update(int[] a, int index, int value) {
+        if (index < 0 || index >= a.length)
+            System.out.println("Index is not found");
+        else
+            a[index] = value;
+
+    }
+    public void delete(int[] a, int index) {
+        if (index < 0 || index >= a.length)
+            System.out.println("Index is not found");
+        else
+            a[index] = 0;
+    }
+    public int LinearSearch(int[]a,int s_value){
+        for(int i=0; i<a.length;i++ ){
+            if(s_value==a[i])
+                return i;
+        }
+        return -1;
+
+    }
+
+    public int BinarySearch(int[]a , int f_index, int l_index, int s_value){
+        while(f_index<=l_index){
+            int mid =(f_index+l_index)/2;
+            System.out.println("mid="+mid);
+
+            if(s_value== a[mid])
+                return mid;
+            if(s_value> a[mid])
+                f_index=mid+1;
+            if (s_value< a[mid])
+                l_index=mid-1;
+        }
+
+        return -1;
+    }
+
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,14 +1,31 @@
 package Arrays;
+
+
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Arrayss arr = new Arrayss();
-        int []number={1,2,3,4,5};
+        int []number={1,2,3,4,5,6,7,8,9,10,12,11};
+        Arrays.sort(number);
+
+        number[3]=7;
         int []number2=new int[5];
         arr.input(number2);
         arr.Traversal(number);
         arr.Traversal2(number);
         arr.ReverseTraversal(number);
         arr.Traversal(number2);
+        arr.update(number,2,7);
+        arr.delete(number, 1);
+
+        arr.LinearSearch(number, 1);
+
+        int result = arr.BinarySearch(number, 0,number.length-1,4);
+        if(result!= -1)
+            System.out.println("Value found at index : "+ result);
+        else
+            System.out.println("Value not found");
         //System.out.println(Arrays.toString(number))
+
     }
 }
