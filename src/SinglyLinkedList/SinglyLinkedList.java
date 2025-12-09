3f7e18c7876fbaea502400cb0ac69b4a419bdd21
Marya <maryaoaidain@gmail.com>
2025-12-09 16:56:55 +0300

    lab4-SinglyLinkedList

--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -1,8 +1,8 @@
 package SinglyLinkedList;
 
 public class SinglyLinkedList<E> {
-    private Node<E>head=null;
-    private Node<E>tail=null;
+    private Node<E> head=null;
+    private Node<E> tail=null;
     private int size=0;
 
 
@@ -10,16 +10,97 @@ public class SinglyLinkedList<E> {
         return size;
     }
 
-    public boolean IsEmpty(){
+    public boolean isEmpty(){
         return size()==0;
     }
 
-    class Node<E>{
+
+    public void addFirst(E data){
+        Node<E> newNode= new Node(data,head);
+        head=newNode;
+        if (isEmpty())
+            tail=head;
+        size++;
+
+    }
+    public E getFirst(){
+        if (isEmpty())return null;
+        return head.getData();
+    }
+    public E removeFirst(){
+        if(isEmpty())return null;
+        E delete= head.getData();
+        head=head.getNext();
+        size--;
+        if (isEmpty())
+            tail=head;
+
+        return delete;
+    }
+    public void addLast(E data){
+        Node newnode= new Node(data,null);
+        if(isEmpty()) {
+            head = newnode;
+        }
+        else {
+            tail.setNext(newnode);
+        }
+        tail=newnode;
+        size++;
+
+    }
+    public E getLast(){
+        if(isEmpty()) {
+            return null;
+        }
+        return tail.getData();
+
+
+    }
+
+    public E removeLast(){
+        if (isEmpty())return null;
+        E delete=tail.getData();
+        if(head==tail){
+            head=null;
+            tail=null;
+        }
+        else{
+            Node<E> temp=head;
+            while (temp.getNext()!=tail){
+                temp=temp.getNext();}
+
+            temp.setNext(null);
+            tail=temp;
+        }
+        size--;
+        return delete;
+    }
+    public void display(){
+        Node<E> temp=head;
+        while (temp!=null){
+            System.out.print(temp.getData()+"----->");
+            temp=temp.getNext();
+        }
+        System.out.println("null \n");
+
+    }
+
+    class Node<E> {
         private E data;
         private Node<E> next;
 
-        public Node(Node<E> next, E data) {
+        public Node(E data, Node<E> next) {
+            this.data = data;
             this.next = next;
+        }
+
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
             this.data = data;
         }
 
@@ -27,16 +108,9 @@ public class SinglyLinkedList<E> {
             return next;
         }
 
-        public E getData() {
-            return data;
-        }
-
         public void setNext(Node<E> next) {
             this.next = next;
         }
-
-        public void setData(E data) {
-            this.data = data;
-        }
     }
+
 }
